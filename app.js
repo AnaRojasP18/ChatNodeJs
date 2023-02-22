694dc35e7ba442eebde13ee809fe99ab95f73c46
AnaRojasP18 <ana.rojas7328@alumnos.udg.mx>
2023-02-21 23:37:10 -0600

    Commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+
+const http = require('http')
+const server = http.createServer(app)
+
+const {Server} = require('socket.io')
+const io = new Server(server)
+
+io.on('connection',(socket)=>{
+    //console.log('Un usuario se ha conectado')
+    /*socket.on('chat',(msg)=>{
+        console.log('Mensaje: '+msg)
+    })*/
+
+    socket.on('chat',(msg)=>{
+        io.emit('chat',msg)
+    })
+})
+
+app.get('/', (req,res)=>{
+   // console.log(__dirname)
+ res.sendFile(`${__dirname}/cliente/index.html`)
+   // res.send('<h1>Aplicación de Chat</h1>')
+})
+server.listen(3000,()=>{
+    console.log('Servidor corriendo en http://localhost:3000')
+})
